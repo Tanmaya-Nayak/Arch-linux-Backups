352017bae0d6aa3212c9db71ed7889ec23bd8d9e
Tanmaya-Nayak <saitanmayanayak2006@gmail.com>
2026-05-17 16:23:43 +0530

    feat: update wallpaper to arch linux 4k using swaybg

--- hypr/wallpaper.sh
+++ hypr/wallpaper.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 pkill swaybg 2>/dev/null
-swaybg -i /home/xstream/Pictures/anime/1345576.jpeg -m fill &
-
-# Update hyprlock
-sed -i "s|path = .*|path = /home/xstream/Pictures/anime/1345576.jpeg|" ~/.config/hypr/hyprlock.conf
+swaybg -i /home/xstream/Pictures/Wallpapers/archwall.jpg -m fill &
+sed -i "s|path = .*|path = /home/xstream/Pictures/Wallpapers/archwall.jpg|" ~/.config/hypr/hyprlock.conf
