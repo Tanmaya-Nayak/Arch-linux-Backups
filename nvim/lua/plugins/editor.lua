98a801464f4bd2bb9f809c060097bdf2b429b19d
Tanmaya-Nayak <saitanmayanayak2006@gmail.com>
2026-05-20 23:29:26 +0530

    feat: add gitsigns toggle signs keybinding

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -10,21 +10,21 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts  = {
-      plugins  = { spelling = { enabled = true } },
-      icons    = { breadcrumb = "»", separator = "➜", group = "+" },
-      win      = { border = "rounded" },
-      spec     = {
-        { "<leader>b",  group = "buffer" },
-        { "<leader>c",  group = "code" },
-        { "<leader>d",  group = "debug" },
-        { "<leader>f",  group = "find/file" },
-        { "<leader>g",  group = "git" },
-        { "<leader>j",  group = "java" },
-        { "<leader>l",  group = "lazy" },
-        { "<leader>m",  group = "mason" },
-        { "<leader>s",  group = "split/swap" },
-        { "<leader>t",  group = "terminal" },
-        { "<leader>w",  group = "workspace/window" },
+      plugins = { spelling = { enabled = true } },
+      icons   = { breadcrumb = "»", separator = "➜", group = "+" },
+      win     = { border = "rounded" },
+      spec    = {
+        { "<leader>b", group = "buffer" },
+        { "<leader>c", group = "code" },
+        { "<leader>d", group = "debug" },
+        { "<leader>f", group = "find/file" },
+        { "<leader>g", group = "git" },
+        { "<leader>j", group = "java" },
+        { "<leader>l", group = "lazy" },
+        { "<leader>m", group = "mason" },
+        { "<leader>s", group = "split/swap" },
+        { "<leader>t", group = "terminal" },
+        { "<leader>w", group = "workspace/window" },
       },
     },
   },
@@ -32,19 +32,19 @@ return {
   -- ── Autopairs ─────────────────────────────────────────────
   {
     "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    opts  = {
-      check_ts        = true,    -- use treesitter
-      ts_config       = { lua = { "string" }, java = false },
-      fast_wrap       = {
-        map    = "<M-e>",
-        chars  = { "{", "[", "(", '"', "'" },
-        pattern= [=[[%'%"%>%]%)%}%,]]=],
-        end_key= "$",
-        keys   = "qwertyuiopzxcvbnmasdfghjkl",
-        check_comma  = true,
-        manual_position= true,
-        highlight      = "Search",
+    event  = "InsertEnter",
+    opts   = {
+      check_ts  = true,       -- use treesitter
+      ts_config = { lua = { "string" }, java = false },
+      fast_wrap = {
+        map             = "<M-e>",
+        chars           = { "{", "[", "(", '"', "'" },
+        pattern         = [=[[%'%"%>%]%)%}%,]]=],
+        end_key         = "$",
+        keys            = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma     = true,
+        manual_position = true,
+        highlight       = "Search",
       },
     },
     config = function(_, opts)
@@ -88,17 +88,18 @@ return {
         local map = function(mode, l, r, desc)
           vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
         end
-        map("n", "]g",          gs.next_hunk,                  "Next hunk")
-        map("n", "[g",          gs.prev_hunk,                  "Prev hunk")
-        map("n", "<leader>gs",  gs.stage_hunk,                 "Stage hunk")
-        map("n", "<leader>gr",  gs.reset_hunk,                 "Reset hunk")
-        map("n", "<leader>gS",  gs.stage_buffer,               "Stage buffer")
-        map("n", "<leader>gu",  gs.undo_stage_hunk,            "Undo stage hunk")
-        map("n", "<leader>gR",  gs.reset_buffer,               "Reset buffer")
-        map("n", "<leader>gp",  gs.preview_hunk,               "Preview hunk")
-        map("n", "<leader>gb",  function() gs.blame_line({ full = true }) end, "Blame line")
-        map("n", "<leader>gd",  gs.diffthis,                   "Diff this")
-        map("n", "<leader>gtb", gs.toggle_current_line_blame,  "Toggle blame")
+        map("n", "]g", gs.next_hunk, "Next hunk")
+        map("n", "[g", gs.prev_hunk, "Prev hunk")
+        map("n", "<leader>gs", gs.stage_hunk, "Stage hunk")
+        map("n", "<leader>gr", gs.reset_hunk, "Reset hunk")
+        map("n", "<leader>gS", gs.stage_buffer, "Stage buffer")
+        map("n", "<leader>gu", gs.undo_stage_hunk, "Undo stage hunk")
+        map("n", "<leader>gR", gs.reset_buffer, "Reset buffer")
+        map("n", "<leader>gp", gs.preview_hunk, "Preview hunk")
+        map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame line")
+        map("n", "<leader>gd", gs.diffthis, "Diff this")
+        map("n", "<leader>gtb", gs.toggle_current_line_blame, "Toggle blame")
+        map("n", "<leader>gts", gs.toggle_signs, "Toggle signs")
       end,
     },
   },
@@ -108,12 +109,12 @@ return {
     "numToStr/Comment.nvim",
     event = { "BufReadPost", "BufNewFile" },
     opts  = {
-      padding   = true,
-      sticky    = true,
-      toggler   = { line = "gcc", block = "gbc" },
-      opleader  = { line = "gc",  block = "gb"  },
-      extra     = { above = "gcO", below = "gco", eol = "gcA" },
-      mappings  = { basic = true, extra = true },
+      padding  = true,
+      sticky   = true,
+      toggler  = { line = "gcc", block = "gbc" },
+      opleader = { line = "gc", block = "gb" },
+      extra    = { above = "gcO", below = "gco", eol = "gcA" },
+      mappings = { basic = true, extra = true },
     },
   },
 
@@ -131,26 +132,28 @@ return {
     version = "*",
     keys    = {
       { "<leader>tt", "<cmd>ToggleTerm direction=horizontal<CR>", desc = "Terminal (horizontal)" },
-      { "<leader>tf", "<cmd>ToggleTerm direction=float<CR>", desc = "Terminal (float)" },
+      { "<leader>tf", "<cmd>ToggleTerm direction=float<CR>",      desc = "Terminal (float)" },
     },
     opts    = {
-      size = function(term)
-        if term.direction == "horizontal" then return 15
-        elseif term.direction == "vertical" then return vim.o.columns * 0.4
+      size            = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
         end
       end,
-      open_mapping   = [[<C-\>]],
-      hide_numbers   = true,
-      shade_terminals= true,
-      shading_factor = 2,
-      start_in_insert= true,
-      persist_mode   = true,
-      direction      = "float",
-      close_on_exit  = true,
-      shell          = vim.o.shell,
-      float_opts     = { border = "curved", winblend = 0 },
+      open_mapping    = [[<C-\>]],
+      hide_numbers    = true,
+      shade_terminals = true,
+      shading_factor  = 2,
+      start_in_insert = true,
+      persist_mode    = true,
+      direction       = "float",
+      close_on_exit   = true,
+      shell           = vim.o.shell,
+      float_opts      = { border = "curved", winblend = 0 },
     },
-    config = function(_, opts)
+    config  = function(_, opts)
       require("toggleterm").setup(opts)
       -- LazyGit integration
       local Terminal = require("toggleterm.terminal").Terminal
@@ -174,14 +177,14 @@ return {
     cmd          = { "Trouble" },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys         = {
-      { "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>",                    desc = "Diagnostics (Trouble)" },
-      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>",       desc = "Buffer diagnostics" },
-      { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<CR>",            desc = "Symbols (Trouble)" },
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>",                        desc = "Diagnostics (Trouble)" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>",           desc = "Buffer diagnostics" },
+      { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<CR>",                desc = "Symbols (Trouble)" },
       { "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<CR>", desc = "LSP (Trouble)" },
-      { "<leader>xL", "<cmd>Trouble loclist toggle<CR>",                        desc = "Location list" },
-      { "<leader>xQ", "<cmd>Trouble qflist toggle<CR>",                         desc = "Quickfix list" },
+      { "<leader>xL", "<cmd>Trouble loclist toggle<CR>",                            desc = "Location list" },
+      { "<leader>xQ", "<cmd>Trouble qflist toggle<CR>",                             desc = "Quickfix list" },
     },
-    opts = {
+    opts         = {
       modes = {
         lsp = { win = { position = "right" } },
       },
@@ -194,12 +197,12 @@ return {
     cmd          = { "TodoTrouble", "TodoTelescope" },
     event        = { "BufReadPost", "BufNewFile" },
     dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-      { "]t",          function() require("todo-comments").jump_next() end, desc = "Next todo" },
-      { "[t",          function() require("todo-comments").jump_prev() end, desc = "Prev todo" },
-      { "<leader>ft",  "<cmd>TodoTelescope<CR>",                           desc = "Telescope todos" },
+    keys         = {
+      { "]t",         function() require("todo-comments").jump_next() end, desc = "Next todo" },
+      { "[t",         function() require("todo-comments").jump_prev() end, desc = "Prev todo" },
+      { "<leader>ft", "<cmd>TodoTelescope<CR>",                            desc = "Telescope todos" },
     },
-    opts = {},
+    opts         = {},
   },
 
   -- ── Mini extras ───────────────────────────────────────────
@@ -223,8 +226,8 @@ return {
     "RRethy/vim-illuminate",
     event  = { "BufReadPost", "BufNewFile" },
     opts   = {
-      delay   = 200,
-      large_file_cutoff = 2000,
+      delay                = 200,
+      large_file_cutoff    = 2000,
       large_file_overrides = { providers = { "lsp" } },
     },
     config = function(_, opts)
